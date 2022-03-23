038e60a2bb732e7e463404bad513a076b3f7237e
fleutius <91052945+fleutius@users.noreply.github.com>
2022-03-23 09:00:37 +0100

    Update bar_roev.js

--- scripts/bar_roev.js
+++ scripts/bar_roev.js
@@ -1,3 +1,4 @@
+document.getElementById("button").onclick = rollDice;
 class Dice {
     constructor(name) {
         this.name = name;
@@ -17,7 +18,7 @@ class Dice {
 
     rollDie() {
         if (this.locked == false) {
-            this.value = Math.floor(Math.random() * 6) + 1;
+            return this.value = Math.floor(Math.random() * 6) + 1;
         }
     }
         
@@ -29,76 +30,6 @@ class Dice {
 
 
 
-function rollDice() {
-    var d1 = new Dice("d1");
-    var d2 = new Dice("d2");
-    var d3 = new Dice("d3");
-    var d4 = new Dice("d4");
-    var d5 = new Dice("d5");
-    var d6 = new Dice("d6");
-
-    const diceList = [d1, d2, d3, d4, d5, d6];
-    activeDice = [];
-    prefix = "die";
-
-
-    for (let i = 0; i < diceList.lenght+1; i++) {
-        if (diceList[i].locked == false) {
-            activeDice.push(diceList[i]);
-            diceList[i].rollDie;
-            var temp = document.getElementById("die"+i);
-            temp.innerHTML = diceList[i].value;
-        }
-        else {
-            diceList[i].rollDie();
-            this["die"+i] = document.getElementById("die"+i);
-            this["die"+i].innerHTML = diceList[i].value;}
-
-
-    }
-
-}
-   
-    /*
-    const diceList = [d1.name, d2.name, d3.name, d4.name, d5.name, d6.name];
-    let activeDice = diceList.filter(function(value){
-        if (value.locked == false){
-            return value;
-        }
-
-    let dLen = activeDice.length;
-    
-    for (let i = 0; i < dLen; i++) {
-
-    }
-    activeDice.forEach(function(value){
-        var die+i++:
-
-        value.rollDie;
-        if (value.value == 2){
-            value.innerHTML = value.value;
-            value.style.backgroundColor = 'red';
-            value.toggle();
-        }
-        else if (value.value == 5){
-            value.innerHTML = value.value;
-            value.style.backgroundColor = 'red';
-            value.toggle();
-        }
-        else {
-            value.innerHTML = value.value;
-        }
-    })
-    });
-    var die1 = document.getElementById("die1");
-    die1.innerHTML = d1.value;
-    
-
-}
-*/
-console.log("-----");
-/*
-document.getElementById("button").onclick = rollDice;
 function rollDice() {
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
@@ -125,23 +56,28 @@ function rollDice() {
     die5.innerHTML = d5;
     die6.innerHTML = d6;
     status.innerHTML = "You rolled "+diceTotal+".";
-    var diceList = [die1, die2, die3, die4, die5, die6]  
-    
-    function checkValue {
-        var el;
-        var prefix = 'die';
-        for (var i = 0; el = document.getElementById(prefix + i); ++1) {
+}
 
-        }
-    }
 
-    diceList.forEach(checkValue);
-    
-    function checkValue(item) {
-        if (item == 2) {
-            item.style.backgroundColor = "red";
-        } else if (working != 2) {
-            item.style.backgroundColor = "lightgray";
+
+/*
+    const diceList = [d1, d2, d3, d4, d5, d6];
+    activeDice = [];
+    prefix = "die";
+
+
+    for (let i = 0; i < diceList.lenght+1; i++) {
+        if (diceList[i].locked == false) {
+            activeDice.push(diceList[i]);
+            diceList[i].rollDie;
+            this["die"+i] = document.getElementById("die"+i);
+            this["die"+i].innerHTML = diceList[i].value;
         }
+        else {
+            diceList[i].rollDie();
+            this["die"+i] = document.getElementById("die"+i);
+            this["die"+i].innerHTML = diceList[i].value;}
+
+
     }
-    */
+*/
